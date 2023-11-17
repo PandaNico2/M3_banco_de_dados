e96856325e69e9be4020453f621c8e5619c52b94
isadora mello <isadora.mello@gmail.com.br>
2023-11-17 13:16:29 -0300

    Arrumei editar classificação

--- importar/editar/editar_classificacao.php
+++ importar/editar/editar_classificacao.php
@@ -1,25 +1,20 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/m3_banco_de_dados/conexao.php');
 
+$id = filter_input(INPUT_POST, 'id_classificacao');
 $num_estrelas = filter_input(INPUT_POST, 'num_estrelas');
 
-if ($num_estrelas) {
-    try {
-        $sql = $conn->prepare("INSERT INTO classificacao (num_estrelas) VALUES (?)");
 
-        $sql->bind_param('s', $num_estrelas);
-        
-        $sql->execute();
-        
-        header('Location: /m3_banco_de_dados/telas_ver/ver_classificacao.php');
-        exit;
-    } catch (PDOException $e) {
-        echo "Erro ao inserir classificacao: " . $e->getMessage();
-        header('Location: /m3_banco_de_dados/telas_add/add_classificacao.php');
-        exit;
-    }
+if ($id && $num_estrelas) {
+    $sql = $conn->prepare("UPDATE classificacao SET num_estrelas = ? WHERE id_classificacao = ?");
+    $sql->bind_param('si', $num_estrelas, $id);
+    $sql->execute();
+    $sql->close();
+
+    header('Location: /m3_banco_de_dados/telas_ver/ver_classificacao.php');
+    exit;
 } else {
-    echo "ERRO! Preencha todos os campos.";
+    header('Location: /m3_banco_de_dados/telas_ver/ver_classificacao.php');
     exit;
 }
 ?>
